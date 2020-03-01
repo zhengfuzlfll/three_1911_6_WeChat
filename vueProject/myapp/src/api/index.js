3ca14bf5dca136928df260e187e567f9e8b87871
zhengfuzlfll <3250856211@qq.com>
2020-03-01 16:52:20 +0800

    Vue--封装数据请求axios

--- vueProject/myapp/src/api/index.js
+++ vueProject/myapp/src/api/index.js
@@ -9,3 +9,37 @@ const isDev = process.env.NODE_ENV === "development";
 
 // 如果是开发环境，服务器可能是本地的，也可能是服务器上的
 const baseUrl=isDev ? "http://daxun.kuboy.top/api":"http://daxun.kuboy.top/api";
+
+//2、自定义axios,添加axios拦截器
+let instance = axios.create({
+    baseURL: baseURL
+  });
+
+let token=localStorage.getItem("token")
+// 在实例已创建后修改默认值,比如请求时需要拿到token值,每次请求时则将token发送到服务器
+// instance.defaults.headers.common['token'] = token // 
+
+//自定义超时时间
+instance.defaults.timeout = 6000
+
+// 拦截器
+// 每一次发送请求前都需要经过这里  添加请求拦截器
+instance.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么-----显示loading动画，携带token信息
+    config.headers['token'] = token // 请求时再携带token
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
+
+// 每一次拿到数据都经过这里    添加响应拦截器
+instance.interceptors.response.use(function (response) {
+    // 对响应数据做点什么 ----关闭loading动画
+    return response;
+}, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+});
+
+export default instance
